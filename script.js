3be01cb8157aa895e6df69930c46605a89a786c7
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-06-18 20:39:25 +0530

    Added tasks to local storage sequential even after page reload

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const addTaskBtn = document.querySelector("#add-task-btn");
 const input = document.querySelector("#add-task");
-const allTasks = [];
-let taskCount = 0;
+const taskContainer = document.querySelector(".tasks");
+let taskCount = parseInt(localStorage.key(""));
+if (NaN) {
+    taskCount = 0;
+}
+
 
 addTaskBtn.addEventListener('click', (e) => {
-    // get task from user
-    const task = input.value;
-
-    // add task to storage
+    const task = input.value; // get task from user
     taskCount++;
-    addToStorage(taskCount, task);
-
-
-    // console.log(task);
+    addToStorage(taskCount, task); // add task to storage
+    showTaskInApp(task)
     e.preventDefault();
 });
 
 function addToStorage(key, value) {
     localStorage.setItem(key, value);
+    console.log("Database updated");
 }
 
+function showTaskInApp(name) {
+    const div = document.createElement("div");
+    div.className = "row";
+    taskContainer.appendChild(div);
+    const checkBox = document.createElement("input");
+    checkBox.type = 'checkbox';
+    div.appendChild(checkBox);
+    const taskName = document.createElement("span");
+    taskName.textContent = localStorage.getItem(taskCount);
+    div.appendChild(taskName);
+}
