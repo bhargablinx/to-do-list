b09eecac891bb530fc58a5e48722bd5d9b980b8a
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-07-08 18:33:15 +0530

    simple terminal based to-do

---
+++ README.md
@@ -0,0 +1,5 @@
+Things that tasks should contain:
+- Name
+- Created Date
+- Due Date
+- Project
---
+++ script.js
@@ -0,0 +1,74 @@
+let taskArray = [];
+
+function createTask(name, duDate, status) {
+    this.name = name;
+    this.duDate = duDate;
+    this.status = status;
+    this.add = function addToArray() {
+        taskArray.push(this);
+    }
+}
+
+function updateArray() {
+    taskArray.forEach((element) => {
+        if (element.status === 'completed') {
+            removeItemOnce(taskArray, element)
+        }
+    });
+}
+
+function removeItemOnce(arr, value) {
+    let index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+}
+
+// DOM
+function app() {
+    const addBtn = document.querySelector('.add-btn');
+    addBtn.addEventListener('click', (e) => {
+        addTask();
+        e.preventDefault();
+    });
+     
+}
+
+// ADDING TASK
+function addTask() {
+    const input = document.querySelector('.input-field');
+    let taskName = input.value;
+    let task = new createTask(taskName, 'due date', 'not started')
+    task.add();
+    pushToStorage();
+}
+
+function pushToStorage() {
+    localStorage.setItem('tasks', JSON.stringify(taskArray))
+}
+
+// ONE PAGE LOAD & SHOW TASKS FROM Local Storage
+function loadTaskFromStorage() {
+    taskArray = JSON.parse(localStorage.getItem('tasks'));
+}
+
+
+let task = new createTask('Wash', '2024-07-15', 'not started');
+task.add();
+task = new createTask('Clean', '2024-07-15', 'not started');
+task.add();
+task = new createTask('Delete Useless Task', '2024-07-15', 'not started');
+task.add();
+
+app()
+
+window.onload = function () {
+    const body = document.querySelector('body')
+    loadTaskFromStorage()
+    taskArray.forEach((item) => {
+        const div = document.createElement('div');
+        div.textContent = item.name;
+        body.appendChild(div);
+    });  
+}
