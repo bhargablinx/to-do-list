4c298b5e8afa9df331e25ff9e7460bdcf70cb059
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-07-08 21:56:17 +0530

    clean code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let taskArray = [];
 
+loadTaskFromStorage();
+
 function createTask(name, duDate, status) {
     this.name = name;
     this.duDate = duDate;
@@ -17,14 +19,6 @@ function updateArray() {
     });
 }
 
-function removeItemOnce(arr, value) {
-    let index = arr.indexOf(value);
-    if (index > -1) {
-      arr.splice(index, 1);
-    }
-    return arr;
-}
-
 // DOM
 function app() {
     const addBtn = document.querySelector('.add-btn');
@@ -35,40 +29,63 @@ function app() {
      
 }
 
+// ---BREAKDOWN---
+
 // ADDING TASK
 function addTask() {
     const input = document.querySelector('.input-field');
     let taskName = input.value;
-    let task = new createTask(taskName, 'due date', 'not started')
+    let task = new createTask(taskName, 'due date', 'not started');
     task.add();
     pushToStorage();
+    showTaskInDOM(taskName);
 }
 
+// SHOW ADDED SHOW
+function showTaskInDOM(taskName) {
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    div.textContent = taskName;
+    body.appendChild(div);
+}
+
+// STORAGE AREAS
 function pushToStorage() {
     localStorage.setItem('tasks', JSON.stringify(taskArray))
 }
 
-// ONE PAGE LOAD & SHOW TASKS FROM Local Storage
 function loadTaskFromStorage() {
     taskArray = JSON.parse(localStorage.getItem('tasks'));
 }
 
-
-let task = new createTask('Wash', '2024-07-15', 'not started');
-task.add();
-task = new createTask('Clean', '2024-07-15', 'not started');
-task.add();
-task = new createTask('Delete Useless Task', '2024-07-15', 'not started');
-task.add();
-
-app()
-
-window.onload = function () {
-    const body = document.querySelector('body')
-    loadTaskFromStorage()
-    taskArray.forEach((item) => {
-        const div = document.createElement('div');
-        div.textContent = item.name;
-        body.appendChild(div);
-    });  
+// UTILS
+function removeItemOnce(arr, value) {
+    let index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
 }
+
+// ONE PAGE LOAD & SHOW TASKS FROM Local Storage
+if (taskArray[0] != undefined) {
+    window.onload = function () {
+        const body = document.querySelector('body')
+        loadTaskFromStorage()
+        taskArray.forEach((item) => {
+            const div = document.createElement('div');
+            div.textContent = item.name;
+            body.appendChild(div);
+        });  
+    }
+}
+
+// let task = new createTask('Wash', '2024-07-15', 'not started');
+// task.add();
+// task = new createTask('Clean', '2024-07-15', 'not started');
+// task.add();
+// task = new createTask('Delete Useless Task', '2024-07-15', 'not started');
+// task.add();
+// pushToStorage()
+
+app();
