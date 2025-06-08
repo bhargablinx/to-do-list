3dd2b7673923a31095e05c1084f8660308a0ada6
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-08 07:31:33 +0000

    toggle strick thorugh

--- src/script.js
+++ src/script.js
@@ -24,14 +24,29 @@ function renderTask(task) {
         "flex items-center justify-between border border-slate-800 rounded-lg p-2 my-3";
     div.innerHTML = `
         <div class="flex items-center gap-3">
-            <input type="checkbox" name="" id="uniqueId" />
-            <label for="uniqueId" class="font-semibold">${task.name}</label>
+            <input type="checkbox" name="" id="${task.id}" />
+            <label for="${task.id}" class="font-semibold">${task.name}</label>
         </div>
         <button class="text-xs bg-red-500 p-1 px-2 rounded-4xl cursor-pointer hover:bg-red-700">delete</button>
     `;
     taskContainer.appendChild(div);
 }
 
+function toggleStrickThrough() {
+    document.querySelector(".checkbox").addEventListener("click", (e) => {
+        if (e.target.checked) {
+            e.target.parentElement.childNodes[3].classList.add("line-through");
+            console.log("Completed");
+        } else {
+            e.target.parentElement.childNodes[3].classList.remove(
+                "line-through"
+            );
+        }
+    });
+}
+
 document.querySelector(".add-task-btn").addEventListener("click", () => {
     taskTaskInput();
 });
+
+toggleStrickThrough();
