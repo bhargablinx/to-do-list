1b0a097034486d626166e4acd24e82fb1ff51f6c
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-08 07:54:47 +0000

    delete btn functionality added

--- src/script.js
+++ src/script.js
@@ -13,6 +13,7 @@ function createTask(taskName) {
 
 function taskTaskInput() {
     const taskName = document.querySelector(".task-inputbox").value;
+    if (taskName === "") return;
     if (taskName.trim() !== "") {
         createTask(taskName);
         document.querySelector(".task-inputbox").value = "";
@@ -24,14 +25,23 @@ function renderTask(task) {
     const div = document.createElement("div");
     div.className =
         "flex items-center justify-between border border-slate-800 rounded-lg p-2 my-3";
-
-    div.innerHTML = `
+    if (task.isCompleted) {
+        div.innerHTML = `
         <div class="flex items-center gap-3">
             <input type="checkbox" class="checkbox" id="${task.id}" />
-            <label for="${task.id}" class="font-semibold">${task.name}</label>
+            <label for="${task.id}" class="font-semibold line-through">${task.name}</label>
         </div>
-        <button class="text-xs bg-red-500 p-1 px-2 rounded-4xl cursor-pointer hover:bg-red-700">delete</button>
+        <button class="delete-btn text-xs bg-red-500 p-1 px-2 rounded-4xl cursor-pointer hover:bg-red-700">delete</button>
     `;
+    } else {
+        div.innerHTML = `
+            <div class="flex items-center gap-3">
+                <input type="checkbox" class="checkbox" id="${task.id}" />
+                <label for="${task.id}" class="font-semibold">${task.name}</label>
+            </div>
+            <button class="delete-btn text-xs bg-red-500 p-1 px-2 rounded-4xl cursor-pointer hover:bg-red-700">delete</button>
+        `;
+    }
 
     const checkbox = div.querySelector(".checkbox");
     const label = div.querySelector("label");
@@ -42,6 +52,13 @@ function renderTask(task) {
         } else {
             label.classList.remove("line-through");
         }
+    });
+
+    const deleteBtn = div.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", (e) => {
+        div.remove();
+        const index = allTasks.findIndex((t) => t.id === task.id);
+        if (index !== -1) allTasks.splice(index, 1);
     });
 
     taskContainer.appendChild(div);
