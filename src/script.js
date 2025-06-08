b421e3e821d752f4b41622d04ae8b4f7eb32568e
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-08 07:39:14 +0000

    toggle strick through refactor

--- src/script.js
+++ src/script.js
@@ -13,8 +13,10 @@ function createTask(taskName) {
 
 function taskTaskInput() {
     const taskName = document.querySelector(".task-inputbox").value;
-    createTask(taskName);
-    document.querySelector(".task-inputbox").value = "";
+    if (taskName.trim() !== "") {
+        createTask(taskName);
+        document.querySelector(".task-inputbox").value = "";
+    }
 }
 
 function renderTask(task) {
@@ -22,31 +24,30 @@ function renderTask(task) {
     const div = document.createElement("div");
     div.className =
         "flex items-center justify-between border border-slate-800 rounded-lg p-2 my-3";
+
     div.innerHTML = `
         <div class="flex items-center gap-3">
-            <input type="checkbox" name="" id="${task.id}" />
+            <input type="checkbox" class="checkbox" id="${task.id}" />
             <label for="${task.id}" class="font-semibold">${task.name}</label>
         </div>
         <button class="text-xs bg-red-500 p-1 px-2 rounded-4xl cursor-pointer hover:bg-red-700">delete</button>
     `;
+
+    const checkbox = div.querySelector(".checkbox");
+    const label = div.querySelector("label");
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            label.classList.add("line-through");
+            task.isCompleted = true;
+        } else {
+            label.classList.remove("line-through");
+        }
+    });
+
     taskContainer.appendChild(div);
 }
 
-function toggleStrickThrough() {
-    document.querySelector(".checkbox").addEventListener("click", (e) => {
-        if (e.target.checked) {
-            e.target.parentElement.childNodes[3].classList.add("line-through");
-            console.log("Completed");
-        } else {
-            e.target.parentElement.childNodes[3].classList.remove(
-                "line-through"
-            );
-        }
-    });
-}
-
-document.querySelector(".add-task-btn").addEventListener("click", () => {
-    taskTaskInput();
-});
-
-toggleStrickThrough();
+// ✅ Add task button
+document
+    .querySelector(".add-task-btn")
+    .addEventListener("click", taskTaskInput);
