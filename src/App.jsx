4061f9fb155e7e002fadeb49df1c231fa92147a4
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-20 07:40:33 +0000

    Chnage task type from string -> object

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,34 @@ import Task from "./components/Task.jsx";
 
 function App() {
     const [currentTask, setCurrentTask] = useState("");
-    const [allTasks, setAllTasks] = useState(["Clean card", "Wash cloths"]);
-
-    function handleDelete(indexToRemove) {
-        setAllTasks((prevTasks) =>
-            prevTasks.filter((_, idx) => idx !== indexToRemove)
-        );
-    }
+    const [allTasks, setAllTasks] = useState([
+        { id: crypto.randomUUID(), name: "Clean car" },
+        { id: crypto.randomUUID(), name: "Wash clothes" },
+    ]);
 
     function handleAddTask() {
         if (currentTask.trim() === "") return;
-        setAllTasks((prev) => [...prev, currentTask]);
+
+        const newTask = {
+            id: crypto.randomUUID(),
+            name: currentTask,
+        };
+
+        setAllTasks((prev) => [...prev, newTask]);
         setCurrentTask("");
+    }
+
+    function handleDelete(taskIdToRemove) {
+        setAllTasks((prev) =>
+            prev.filter((task) => task.id !== taskIdToRemove)
+        );
     }
 
     return (
         <div className="bg-slate-900 text-slate-50 flex justify-center items-center h-screen">
             <div className="border border-slate-700 w-[300px] h-fit min-h-[400px] rounded-2xl p-3 px-5">
                 <h1 className="text-center text-xl font-extrabold underline underline-offset-4">
-                    To-Do-List
+                    To-Do List
                 </h1>
                 <div className="my-4 flex gap-3">
                     <input
@@ -39,12 +48,12 @@ function App() {
                     </button>
                 </div>
                 <div className="my-8 text-sm text-slate-200 space-y-3">
-                    {allTasks.map((task, idx) => (
+                    {allTasks.map((task) => (
                         <Task
-                            key={idx}
-                            id={`task-${idx}`}
-                            taskName={task}
-                            onDelete={() => handleDelete(idx)}
+                            key={task.id}
+                            id={task.id}
+                            taskName={task.name}
+                            onDelete={() => handleDelete(task.id)}
                         />
                     ))}
                 </div>
