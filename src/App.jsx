f934ddb490ea667f9b4f865a3ec8aa58a5b146e7
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-20 06:54:14 +0000

    Stores the current task

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+
 function App() {
+    const [currentTask, setCurrentTask] = useState("");
+
     return (
         <div className="bg-slate-900 text-slate-50 flex justify-center items-center h-screen">
-            <div class="border border-slate-700 w-[300px] h-fit min-h-[400px] rounded-2xl p-3 px-5">
+            <div className="border border-slate-700 w-[300px] h-fit min-h-[400px] rounded-2xl p-3 px-5">
                 <h1 className="text-center text-xl font-extrabold underline underline-offset-4">
                     To-Do-List
                 </h1>
                 <div className="my-4 flex gap-3">
                     <input
+                        onChange={(e) => setCurrentTask(e.target.value)}
                         type="text"
                         placeholder="Add Tasks!"
+                        value={currentTask}
                         className="task-inputbox border p-2 text-sm flex-1 rounded-lg focus:outline-1 focus:outline-slate-500 text-slate-400"
                     />
-                    <button className="flex-1 rounded-2xl cursor-pointer text-sm bg-slate-200 text-slate-900 font-semibold hover:text-slate-900 hover:bg-slate-300">
+                    <button
+                        onClick={() => console.log(currentTask)}
+                        className="flex-1 rounded-2xl cursor-pointer text-sm bg-slate-200 text-slate-900 font-semibold hover:text-slate-900 hover:bg-slate-300"
+                    >
                         Add
                     </button>
                 </div>
